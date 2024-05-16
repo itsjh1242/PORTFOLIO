97c3db5015706c077365a26cc1d08d862bd0f807
itsjh <93984898+itsjh1242@users.noreply.github.com>
2024-05-16 20:23:41 +0900

    Update ContextAPI.tsx

--- app/components/ContextAPI.tsx
+++ app/components/ContextAPI.tsx
@@ -4,6 +4,6 @@ const PortfolioContext = React.createContext(null);
 
 export const PortfolioProvider = PortfolioContext.Provider;
 export const PortfolioConsumer = PortfolioContext.Consumer;
-export const prefix = process.env.NODE_ENV === "production" ? "https://itsjh1242.github.io/portfolio" : "";
+export const prefix = process.env.NODE_ENV === "production" ? "/portfolio" : "";
 
 export default PortfolioContext;
